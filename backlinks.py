21a1ab75192cccedf65704fedff99556fc12fa92
Benjamin-Vencill <btvencill@protonmail.com>
2025-01-10 18:48:38 -0500

    Script to link backlinks together

---
+++ backlinks.py
@@ -0,0 +1,46 @@
+"""
+Process all files in `_posts/` and configure their matching backlinks
+"""
+
+import os
+import re
+
+blog_base = "https://benjamin-vencill.github.io/jekyll/update"
+backlink_regex = r'\[\[(.*?)\]\]'
+
+def name_to_backlink(name: str) -> str:
+    """
+    Given a post filename, produce it's backlink title.
+    """
+    return name.split(".")[0].split("-")[-1]
+
+def name_to_url(name: str) -> str:
+    """
+    Given a post filename, produce it's blog url.
+    """
+    path = "/".join(name.split("-"))
+    path = path.replace(".md", ".html")
+    return f"{blog_base}/{path}"
+
+valid_backlinks = {name_to_backlink(name): name_to_url(name) for name in os.listdir("_posts")}
+
+
+print(f"Found {len(valid_backlinks.keys())} backlinks:")
+print("".join([f"{key}:{value}" for key, value in valid_backlinks.items()]))
+
+for post in os.listdir("_posts"):
+    with open(f"_posts/{post}", "r") as f:
+        corpus = f.read()
+
+    backlinks_in_corpus = re.findall(backlink_regex, corpus)
+    print(backlinks_in_corpus)
+    for backlink in backlinks_in_corpus:
+        if backlink in valid_backlinks.keys():
+            target = f"[[{backlink}]]"
+            payload = f"[{backlink}]({valid_backlinks.get(backlink)})"
+            print(f"Replacing {target} with {payload}")
+            corpus = corpus.replace(target, payload)
+    
+    with open(f"_posts/{post}", "w") as f:
+        f.write(corpus)
+
